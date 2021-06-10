b5d06b55ab22aaaf9753e22f06267377da8cb531
JakeJMattson <dev.ioparameters@simplelogin.fr>
2021-06-10 11:55:24 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,26 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 group = "me.moeszyslak"
 version = Versions.BOT
 
 plugins {
     kotlin("jvm") version "1.4.0"
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 repositories {
     mavenCentral()
     jcenter()
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
     implementation("me.jakejmattson:DiscordKt:${Versions.DISCORDKT}")
     implementation("org.apache.tika:tika-parsers:1.24.1")
-    implementation("com.github.kittinunf.fuel:fuel:2.2.3")
-    implementation("com.github.kittinunf.fuel:fuel-gson:2.2.3")
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-gson:2.3.1")
 }
 
-
 tasks {
-    withType<KotlinCompile> {
+    compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
 
@@ -31,13 +28,11 @@ tasks {
         archiveFileName.set("Taboo.jar")
         manifest {
             attributes(
-                    "Main-Class" to "me.moeszyslak.taboo.MainAppKt"
+                "Main-Class" to "me.moeszyslak.taboo.MainAppKt"
             )
         }
     }
 }
-
-
 
 object Versions {
     const val BOT = "1.0.0"
