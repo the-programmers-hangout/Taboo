809953189e28407022766f9c233e29aff524779e
moe <moe9999@protonmail.com>
2021-08-15 11:49:33 +0100

    version bump

--- src/main/kotlin/me/moeszyslak/taboo/MainApp.kt
+++ src/main/kotlin/me/moeszyslak/taboo/MainApp.kt
@@ -82,7 +82,7 @@ suspend fun main() {
 
                 name = "Bot Info"
                 value = "```" +
-                        "Version: 2.1.1\n" +
+                        "Version: 2.1.2\n" +
                         "DiscordKt: ${versions.library}\n" +
                         "Kord: ${versions.kord}\n" +
                         "Kotlin: ${versions.kotlin}" +
